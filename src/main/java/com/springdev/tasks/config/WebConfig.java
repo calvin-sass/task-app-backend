24cbfcc0628428d55a17f529e8b7304c6c4b85da
Calvin Sass <calvinsass@outlook.com>
2025-05-10 17:09:39 +0200

    Added a WebConfig package and Class

---
+++ src/main/java/com/springdev/tasks/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.springdev.tasks.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://task-list-app-frontend.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
